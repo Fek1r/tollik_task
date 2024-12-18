f2ecdd068af488ad9f7112e640e9e7582da74dde
SerGay <stivkivi@gmail.com>
2024-12-18 22:39:07 +0200

    Fix

---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./main.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="register-title">Register</h1>
+      <section className="input-box">
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
+        <i className="bx bxs-user"></i>
+      </section>
+      <section className="input-box">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <i className="bx bxs-envelope"></i>
+      </section>
+      <section className="input-box">
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <i className="bx bxs-lock-alt"></i>
+      </section>
+      <section className="input-box">
+        <input
+          type="password"
+          name="confirm-password"
+          placeholder="Confirm Password"
+        />
+        <i className="bx bxs-lock-alt"></i>
+      </section>
+      <button className="register-button" type="submit">
+        Register
+      </button>
+      <h5 className="already-have-an-account">
+        Already have an account?{" "}
+        <a href="#">
+          <b>Login</b>
+        </a>
+      </h5>
+    </div>
+  );
+}
+
+export default App;
